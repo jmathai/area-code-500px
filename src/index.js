8c1580ff0216cfd1fa50e3cea6de6cd0eacab39f
Jaisen Mathai <jaisen@jmathai.com>
2016-03-17 00:31:24 -0700

    Initial working version

--- src/index.js
+++ src/index.js
@@ -7,45 +7,63 @@
  * The joule-node-response module helps format responses properly.
  */
 var Response = require('joule-node-response');
-
-/*
- * The nodejs library by Twilio for their API.
- * This Joule exports the account SID and TOKEN into the environment.
- *
- * Environment Variables:
- *  - TWILIO_ACCOUNT_SID, Your Twilio account sid.
- *  - TWILIO_AUTH_TOKEN, Your Twilio auth token.
- *  - from, This must be your Twilio phone number.
- *
- * DO NOT INCLUDE SECRETS INTO THIS REPOSITORY.
- */
-var client = require('twilio')();
+var Users = require('./users');
+var Client = require('./client');
 
 exports.handler = function(event, context) {
-  var response = new Response();
+  var response = new Response()
+      , client = new Client()
+      , users = new Users()
+      , component = event.path[0] || null;
+
   response.setContext(context);
-  /*
-   *  event.body, The message to be sent in the SMS.
-   *  event.to, The phone number the message should be sent to.
-   */
-  client.messages.create({
-    body: event.post['body'],
-    to: event.post['to'],
-    from: process.env.FROM
-  }, function(err, message) {
-    /*
-     * This is the function which is called when the API call to send an SMS completes.
-     * We initialize the Response module so we can return a success or failure response.
-     */
-    if(err) {
-      response.setHttpStatusCode(400);
-      response.send({error: err});
-      return;
-    } else if(message && message.sid) {
-      response.send({messageSid: message.sid});
-      return;
-    }
-    response.setHttpStatusCode(500);
-    response.send({error: 'unknown'});
-  });
+
+
+  users.init()
+    .done(function(userList) {
+      users.initUsers(userList);
+      var userStatus = users.getUserStatus(event.query['From']);
+
+      switch(component) {
+        case 'reset':
+          users.reset()
+            .done(function(data) {
+              response.send('Reset');
+            });
+          break;
+        default:
+          // if the body is "start" we register the user
+          // else we check if the user has a name
+          //  if the user is regisered but does not have a name we assume the Body is the name and store it
+          //  else if the user has a name we send the Body to the list
+          if(event.query['Body'].toLowerCase().trim() === 'start') {
+            users.start(event.query['From'])
+              .done(function(data) {
+                client.send(event.query['From'], 'Thanks. Please reply with your name.', response);
+              });
+            return;
+          } else {
+            if(userStatus === -1) {
+              client.send(event.query['From'], 'Please subscribe by texting "start" to this number.', response);
+              return;
+            } else if(userStatus === 0) {
+              users.setName(event.query['From'], event.query['Body'].trim())
+                .done(function(data) {
+                  client.send(event.query['From'], 'You\'ve been subscribed to this group chat. Reply to send the group a message.', response);
+                });
+            } else if(userStatus === 1 && event.query['Body'].trim().length > 0) {
+              var promises = [];
+              var pcnt = 0;
+              for(number in userList) {
+                if(number == event.query['From']) {
+                  console.log('Skipping ' + number);
+                  continue;
+                }
+                client.send(number, client.constructBody(userList[event.query['From']], event.query['Body']), null);
+              }
+              response.send('Finished');
+            }
+          }
+      }
+    });
 };
