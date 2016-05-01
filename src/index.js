95f12b7f7f63f02bdb18f7a7c71c2947209ff036
Jaisen Mathai <jaisen@jmathai.com>
2016-05-01 16:26:45 -0700

    Add photobycity endpoint

--- src/index.js
+++ src/index.js
@@ -12,19 +12,39 @@ var Client = require('./client');
 var Client500Px = require('600px');
 var request = require('request');
 var cheerio = require('cheerio');
+var client500Px = new Client500Px({
+      consumer_key: process.env.PX_KEY,
+      consumer_secret: process.env.PX_SECRET,
+      token: process.env.PX_USER_TOKEN,
+      token_secret: process.env.PX_USER_SECRET
+    });
 
+var getPhotoByCityName = function(cityName, jouleResponse) {
+  var imageUrl = 'https://drscdn.500px.org/photo/6361557/m%3D1600_k%3D1_a%3D1/7cd606b858a17da669e55d57c4b3304a'
+      , imageTitle = 'Volker Handke'
+      , imageLink = 'https://500px.com/photo/6361557/new-york-by-volker-handke'
+      , photos;
+
+  photos = client500Px.photos.searchByTerm(cityName, {image_size:'1600', rpp: '1', only: 'City and Architecture,Landscape'})
+            .catch(function() {
+              //jouleResponse.send({imageUrl: imageUrl, imageTitle: imageTitle, imageLink: imageLink});
+            })
+            .then(function(response) {
+              if(response && response['photos'].length > 0) {
+                imageUrl = response['photos'][0]['image_url'];
+                imageTitle = response['photos'][0]['description'] || response['photos'][0]['user']['fullname'];
+                imageLink = 'https://500px.com' + response['photos'][0]['url'];
+              }
+
+              jouleResponse.send({cityName: cityName, imageUrl: imageUrl, imageTitle: imageTitle, imageLink: imageLink});
+            });
+};
 
 exports.handler = function(event, context) {
   var jouleResponse = new Response()
       , client = new Client()
       , users = new Users()
-      , component = event.path[0] || null
-      , client500Px = new Client500Px({
-        consumer_key: process.env.PX_KEY,
-        consumer_secret: process.env.PX_SECRET,
-        token: process.env.PX_USER_TOKEN,
-        token_secret: process.env.PX_USER_SECRET
-      });
+      , component = event.path[0] || null;
   console.log(event);
 
   jouleResponse.setContext(context);
@@ -51,28 +71,14 @@ exports.handler = function(event, context) {
               var doc = cheerio.load(body)
                   //, city = doc('h2');
                   , city = doc('._Oqb')
-                  , cityName = city.text() || 'San Francisco'
-                  , imageUrl = 'https://drscdn.500px.org/photo/6361557/m%3D1600_k%3D1_a%3D1/7cd606b858a17da669e55d57c4b3304a'
-                  , imageTitle = 'Volker Handke'
-                  , imageLink = 'https://500px.com/photo/6361557/new-york-by-volker-handke'
-                  , photos;
-
-              photos = client500Px.photos.searchByTerm(cityName, {image_size:'1600', rpp: '1', only: 'City and Architecture,Landscape'})
-                        .catch(function() {
-                          //jouleResponse.send({imageUrl: imageUrl, imageTitle: imageTitle, imageLink: imageLink});
-                        })
-                        .then(function(response) {
-                          if(response && response['photos'].length > 0) {
-                            imageUrl = response['photos'][0]['image_url'];
-                            imageTitle = response['photos'][0]['description'] || response['photos'][0]['user']['fullname'];
-                            imageLink = 'https://500px.com' + response['photos'][0]['url'];
-                          }
-
-                          jouleResponse.send({cityName: cityName, imageUrl: imageUrl, imageTitle: imageTitle, imageLink: imageLink});
-                        });
+                  , cityName = city.text() || 'San Francisco';
+              getPhotoByCityName(cityName, jouleResponse);
             }
           }
         );
+        break;
+      case 'photobycity':
+        getPhotoByCityName(event.query['cityName'], jouleResponse);
         break;
       case 'iplookup':
         jouleResponse.setContentType('application/json');
